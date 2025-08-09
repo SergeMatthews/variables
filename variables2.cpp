de6fd3fc5269055a218e5ed649aba69bdfc9f219
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-09 11:46:42 +0600

    Add files via upload

---
+++ variables2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string message;
+	cout << "Введите слово:" << endl;
+	cin >> message;
+	cout << "Вы ввели:" << endl << message;
+
+	SetConsoleCP(866);
+	SetConsoleOutputCP(866);
+
+	return 0;
+}
+
