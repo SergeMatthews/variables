1fa168f4e34cf693c2169bf96d62d61d215020cb
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-09 11:46:23 +0600

    Add files via upload

---
+++ variables1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    double number = 0;
+    cout << "Введите число:" << endl;
+    cin >> number;
+    cout << "Вы ввели:" << endl << number << endl;
+
+    return 0;
+}
+
+
